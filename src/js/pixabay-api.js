0df70300b20cfe3d86a849a354e07d5471c18cd0
rubin <rubin666666666@gmail.com>
2026-01-14 12:09:16 +0200

    Initial project setup with vanilla app template

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,5 @@
+// Pixabay API key
+const API_KEY = 'YOUR_PIXABAY_API_KEY';
+const API_URL = 'https://pixabay.com/api/';
+
+export { API_KEY, API_URL };
---
+++ src/js/render-functions.js
@@ -0,0 +1,8 @@
+// Render functions for displaying gallery
+export function renderGallery(images) {
+  // Render gallery images
+}
+
+export function clearGallery() {
+  // Clear gallery
+}
